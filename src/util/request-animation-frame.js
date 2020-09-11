1223e17d5d985b7419e7033b4d441983b941e342
Wonie <woniesong92@gmail.com>
2020-09-11 03:24:10 -0700

    Remove window references

--- src/util/request-animation-frame.js
+++ src/util/request-animation-frame.js
@@ -5,11 +5,5 @@
  *
  * @return {function} Available `requestAnimationFrame` function for the browser
  */
-export default (
-    window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    window.oRequestAnimationFrame ||
-    window.msRequestAnimationFrame ||
-    ((callback, element) => setTimeout(callback, 1000 / 60))
-).bind(window);
+
+export default (callback, element) => setTimeout(callback, 1000 / 60);
